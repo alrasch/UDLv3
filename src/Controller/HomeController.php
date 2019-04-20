beb4b37f1b9bf825e718ae51bdcb52eaddb4b1c5
alrasch <arasch855@gmail.com>
2019-04-21 00:07:28 +0200

    Find Disciplines in DB and generate basic homepage.

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Discipline;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    const INDEX_TEMPLATE = 'home/index.html.twig';
+
+    public function indexAction()
+    {
+        $disciplines = $this->getDoctrine()->getRepository(Discipline::class)->findAll();
+        $data = [
+            'disciplines' => $disciplines
+        ];
+        return $this->render(self::INDEX_TEMPLATE, $data);
+    }
+}
---
+++ src/Entity/Discipline.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Discipline
+ *
+ * @ORM\Table(name="discipline")
+ * @ORM\Entity
+ */
+class Discipline
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false, options={"unsigned"=true})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="short_description", type="text", length=65535, nullable=false)
+     */
+    private $shortDescription;
+
+    /**
+     * @var bool|null
+     *
+     * @ORM\Column(name="sort_weight", type="boolean", nullable=true)
+     */
+    private $sortWeight;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url_slug", type="string", length=50, nullable=false)
+     */
+    private $urlSlug;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="topic", type="integer", nullable=false, options={"unsigned"=true})
+     */
+    private $topic = '0';
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getShortDescription(): ?string
+    {
+        return $this->shortDescription;
+    }
+
+    public function setShortDescription(string $shortDescription): self
+    {
+        $this->shortDescription = $shortDescription;
+
+        return $this;
+    }
+
+    public function getSortWeight(): ?bool
+    {
+        return $this->sortWeight;
+    }
+
+    public function setSortWeight(?bool $sortWeight): self
+    {
+        $this->sortWeight = $sortWeight;
+
+        return $this;
+    }
+
+    public function getUrlSlug(): ?string
+    {
+        return $this->urlSlug;
+    }
+
+    public function setUrlSlug(string $urlSlug): self
+    {
+        $this->urlSlug = $urlSlug;
+
+        return $this;
+    }
+
+    public function getTopic(): ?int
+    {
+        return $this->topic;
+    }
+
+    public function setTopic(int $topic): self
+    {
+        $this->topic = $topic;
+
+        return $this;
+    }
+
+
+}
