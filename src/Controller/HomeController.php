6c86e0136a475fcae65dbc98085b2e9b7ef32271
alrasch <arasch855@gmail.com>
2019-04-25 00:48:59 +0200

    Populate discplines column-first.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,33 @@
 namespace App\Controller;
 
 use App\Entity\Discipline;
+use App\Logic\Discipline\Mapper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     const INDEX_TEMPLATE = 'home/index.html.twig';
 
+    /** @var Mapper */
+    private $mapper;
+
+    public function __construct(Mapper $mapper)
+    {
+        $this->mapper = $mapper;
+    }
+
     public function indexAction()
     {
         $disciplines = $this->getDoctrine()->getRepository(Discipline::class)->findBy([], ['sortWeight' => 'ASC']);
+
+        $items = $this->mapper->mapDisciplines($disciplines);
+        $count = count($items);
+
+        $items = array_chunk($items, ceil($count / 3));
+
         $data = [
-            'disciplines' => $disciplines
+            'disciplines' => $items,
+            'count' => $count,
         ];
         return $this->render(self::INDEX_TEMPLATE, $data);
     }
