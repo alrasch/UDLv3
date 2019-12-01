501af37bc94fd313d8ebc77558bed9682c75ed24
alrasch <arasch855@gmail.com>
2019-12-01 16:08:33 +0100

    Add shortcut for starting mysql container with some base fixtures.

---
+++ docker/mysql/udl_mysql.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker stop udldb
+docker run -it \
+           -e MYSQL_ROOT_PASSWORD=udl \
+           -v $HOME/Dev/UDLv3/docker/mysql/devdb:/docker-entrypoint-initdb.d \
+           --name udldb \
+           udldb
