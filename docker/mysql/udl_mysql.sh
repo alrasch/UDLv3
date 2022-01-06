649ff49f513d2364090e02f74aa29c03949a2524
alrasch <arasch855@gmail.com>
2022-01-06 02:39:56 +0100

    Remove any dangling database docker images before starting a new one from shortcut.

--- docker/mysql/udl_mysql.sh
+++ docker/mysql/udl_mysql.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker stop udldb
+docker rm udldb
 docker run -it \
            -e MYSQL_ROOT_PASSWORD=udl \
            -v $HOME/Dev/UDLv3/docker/mysql/devdb:/docker-entrypoint-initdb.d \
